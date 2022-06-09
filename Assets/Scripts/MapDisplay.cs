8f2860f642a082d96dffba84408d22dc8ccc37d7
Cojocariu Vlad-Andrei <71493814+crova27@users.noreply.github.com>
2022-06-09 19:00:41 +0300

    Finished
    Texture generator finished for 2d color map.

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MapDisplay : MonoBehaviour {
 
     public Renderer textureRender;
-
+    //changed from previous DrawNoiseMap to draw texture in order to be able to render different kind of textures
     public void DrawTexture(Texture2D texture) {
         textureRender.sharedMaterial.mainTexture = texture;
         textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height);
