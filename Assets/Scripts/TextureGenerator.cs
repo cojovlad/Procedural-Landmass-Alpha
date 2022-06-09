0b3971c15a0f9906df55878ee0a6bc0a569224eb
Cojocariu Vlad-Andrei <71493814+crova27@users.noreply.github.com>
2022-06-09 21:58:32 +0300

    Merge
    Merged from TextureGenerator.

--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -6,15 +6,19 @@ public static class TextureGenerator {
     public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height) {
         //creating a 2 texture with size 2*width*height)
         Texture2D texture = new Texture2D (width, height);
+        //fixes bluriness
         texture.filterMode = FilterMode.Point;
+        //fixes being able to see from other side of map
         texture.wrapMode = TextureWrapMode.Clamp;
+        //sets colors
         texture.SetPixels (colorMap);
         texture.Apply ();
         return texture;
     }
     public static Texture2D TextureFromHeightMap(float[,] heightMap) {
-        int width = heightMap.GetLength (0);
-        int height = heightMap.GetLength (1);
+        //method returns number of elements in the row direction in a multidimensional arra
+        int width = heightMap.GetLength (0);//returns rows
+        int height = heightMap.GetLength (1);//returnw columns
 
         Color[] colorMap = new Color[width * height];
         //we iterate through h and w l in order to color the map acordingly to its height and width with a color between black and white
