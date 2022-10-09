f8d536f5e4b41ceac067bccb0d609db71227aa13
Cojocariu Vlad-Andrei <71493814+crova27@users.noreply.github.com>
2022-10-09 15:57:13 +0300

    Fixed bugs.

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -2,24 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class MapDisplay : MonoBehaviour {
-    
 
-    public Renderer textureRender;
-    //getting a public reference
-    public MeshFilter meshFilter;
+	public Renderer textureRender;
+	public MeshFilter meshFilter;
+	public MeshRenderer meshRenderer;
 
-    public MeshRenderer meshRenderer;
-    //changed from previous DrawNoiseMap to draw texture in order to be able to render different kind of textures
-    public void DrawTexture(Texture2D texture) {
-        textureRender.sharedMaterial.mainTexture = texture;
-        textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height);
-    }
-    //displaying mesh
-    public void DrawMesh(MeshData meshData, Texture2D texture )
-    {
-        //we generate outside of gamemode
-        meshFilter.sharedMesh = meshData.CreateMesh();
-        meshRenderer.sharedMaterial.mainTexture = texture;
-    }
-	
+	public void DrawTexture(Texture2D texture) {
+		textureRender.sharedMaterial.mainTexture = texture;
+		textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height);
+	}
+
+	public void DrawMesh(MeshData meshData, Texture2D texture) {
+		meshFilter.sharedMesh = meshData.CreateMesh ();
+		meshRenderer.sharedMaterial.mainTexture = texture;
+	}
+
 }
