c122efdc828b4532c6e042011e7b1e891b2062bc
Cojocariu Vlad-Andrei <71493814+cojovlad@users.noreply.github.com>
2022-10-28 13:10:22 +0300

    Mesh works and added curvature for moutains and such

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -4,18 +4,16 @@ using UnityEditor;
 
 [CustomEditor (typeof (MapGenerator))]
 public class MapGeneratorEditor : Editor {
-    //target is the object that the custom editor is inspecting
-    // we want to cast that object to a map generator
+
     public override void OnInspectorGUI() {
         MapGenerator mapGen = (MapGenerator)target;
 
         if (DrawDefaultInspector ()) {
-            //also generate the map on each change in "MapGenerator"
             if (mapGen.autoUpdate) {
                 mapGen.GenerateMap ();
             }
         }
-        //generate button
+
         if (GUILayout.Button ("Generate")) {
             mapGen.GenerateMap ();
         }
